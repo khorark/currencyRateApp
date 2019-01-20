68b193b7bb24649564543f6fbb9ec78c943ee795
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 19:22:06 +0300

    Field. добавлена возможность удаления валюты.

--- components/CurrencyField/CurrencyField.tsx
+++ components/CurrencyField/CurrencyField.tsx
@@ -8,16 +8,17 @@ interface ICurrencyField {
     name: string
     value: number
     prevValue: number
+    onClickRemove: (code: string) => void
 }
 
-const CurrencyField = ({ id, idx, name, value, prevValue }: ICurrencyField) => (
+const CurrencyField = ({ id, idx, name, value, prevValue, onClickRemove }: ICurrencyField) => (
     <div key={id} className={'field_container'}>
         <div>{idx}</div>
         <div>{name}</div>
         <div>{value}</div>
         <div>{prevValue}</div>
         <div>{(value / prevValue).toFixed(3)}</div>
-        <div />
+        <div className={'icon_remove'} title={'Удалить валюту'} onClick={onClickRemove.bind(null, name)} />
     </div>
 )
 
