7533ea21417c7670d59c73841503a176327608a5
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 19:38:12 +0300

    Добавлена кнопка добавления валюты

---
+++ components/ButtonAdd/ButtonAdd.tsx
@@ -0,0 +1,10 @@
+import './ButtonAdd.less'
+
+const ButtonAdd = ({}) => (
+    <div className={'button_container'}>
+        <div className={'icon_add'} />
+        <div className={'text'}>Добавить валюту</div>
+    </div>
+)
+
+export default ButtonAdd
