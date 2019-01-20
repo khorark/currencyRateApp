b8d591965bdf8485e53491689df549f434db4736
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 19:49:59 +0300

    Header. Добавлено получение текущей даты

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,13 @@ import './HeaderStyles.less'
 const Header = () => (
     <div className={'header_container'}>
         <div className={'title'}>Курс валют</div>
-        <div className={'title_date'}>20.01.2019</div>
+        <div className={'title_date'}>
+            {new Date().toLocaleString('ru', {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+            })}
+        </div>
         <div className={'title_text'}>RUB</div>
     </div>
 )
