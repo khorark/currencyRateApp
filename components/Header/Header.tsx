c46194f30797051d33b9f96e30f2f10d87d1e67e
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 18:24:15 +0300

    Добавлен комопнент для отображения шапки таблицы

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './HeaderStyles.less'
+
+const Header = () => (
+    <div className={'header_container'}>
+        <div className={'title'}>Курс валют</div>
+        <div className={'title_date'}>20.01.2019</div>
+        <div className={'title_text'}>RUB</div>
+    </div>
+)
+
+export default Header
