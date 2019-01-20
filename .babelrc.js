4d4ffde1afb6a38ba8e8d712f796f833fed527b9
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 16:42:46 +0300

    babel. Добавлена поддержка декораторов.

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    presets: ['next/babel', '@zeit/next-typescript/babel']
+    presets: ['next/babel', '@zeit/next-typescript/babel'],
+    plugins: [
+        [
+            "@babel/plugin-proposal-decorators",
+            {
+                "legacy": true
+            }
+        ]
+    ]
  }
