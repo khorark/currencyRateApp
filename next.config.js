04868d7d1ba842e8f8720df592d6bfe10a651647
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 19:37:58 +0300

    Обновлен nextjs конфиг

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withTypescript = require('@zeit/next-typescript')
 const withLess = require('@zeit/next-less')
 
-module.exports = withTypescript(withLess())
+module.exports = withTypescript(
+    withLess({
+        cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: '[local]___[hash:base64:5]',
+        },
+    }),
+)
