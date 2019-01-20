9621acad9b43daa6c14c177d24bebd1547078d21
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-20 18:27:01 +0300

    Добавлен вывод списка с валютами.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React, { PureComponent } from 'react'
 import Head from 'next/head'
 import { bindActionCreators, Dispatch } from 'redux'
+import { connect } from 'react-redux'
 
+import './styles/index.less'
 import { IReduxState, ICurrency } from '../lib/types/reducers'
 import { getData } from '../lib/redux/actions'
-import { connect } from 'react-redux'
+import Header from '../components/Header/Header'
+import CurrencyField from '../components/CurrencyField/CurrencyField'
 
 interface IDispatch {
     getData: () => void
@@ -28,14 +31,31 @@ export default class App extends PureComponent<IAppProps> {
     }
 
     public render() {
-        console.log(this.props)
+        const { data } = this.props
 
         return (
             <div>
                 <Head>
                     <title>NextJS Page title</title>
                 </Head>
-                <div>Hello Next.js</div>
+                <div className={'app_container'}>
+                    <div className={'main_container'}>
+                        <Header />
+                        <div className={'currency_list'}>
+                            <div className={'currency_list_header'}>
+                                <div>№</div>
+                                <div>Валюта</div>
+                                <div>Текущее</div>
+                                <div>Предыдущее</div>
+                                <div>Динамика</div>
+                                <div />
+                            </div>
+                            {data.map(({ ID, CharCode, Value, Previous }, idx) => (
+                                <CurrencyField id={ID} name={CharCode} value={Value} prevValue={Previous} idx={idx} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
