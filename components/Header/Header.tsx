a92665f7089a4528977bec70f37434a6da9c9908
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-21 12:37:29 +0300

    Header. Добавлено получение одинаковой даты как на сервере, так и на клиенте.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,10 @@
 import './HeaderStyles.less'
+import { getNowDate } from '../../lib/helpers/dateHelper'
 
 const Header = () => (
     <div className={'header_container'}>
         <div className={'title'}>Курс валют</div>
-        <div className={'title_date'}>
-            {new Date().toLocaleString('ru', {
-                year: 'numeric',
-                month: 'numeric',
-                day: 'numeric',
-            })}
-        </div>
+        <div className={'title_date'}>{getNowDate()}</div>
         <div className={'title_text'}>RUB</div>
     </div>
 )
